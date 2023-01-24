4f9fe13f1566b2435304d3a1e0ec40a790a57baa
Eloenai Da Silva Cardoso <eloenay@gmail.com>
2023-01-24 20:39:52 -0300

    exercicio

---
+++ Fundamentos/secao-03-introdução-a-javascript/dia-02-array-e-loop-for/arrays.js
@@ -0,0 +1,15 @@
+// Como vimos no vídeo acima, uma das grandes vantagens dos arrays é o armazenamento de várias informações em uma mesma estrutura. Veja o exemplo e pratique:
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+
+// Como posso adicionar e remover itens de um Array? Veja e pratique com o exemplo a seguir:
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+
+
